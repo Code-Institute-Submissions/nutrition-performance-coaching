a801ef003d91c6472e1d89fcc5fa3324a41f1266
Sam <hello@sammcnally.dev>
2021-06-23 21:41:23 +0100

    add: navbar responsiveness

--- components/Navbar.js
+++ components/Navbar.js
@@ -29,58 +29,90 @@ const Navbar = () => {
     <nav
       className={`transition duration-500 ease-in-out transform ${
         scroll ? "bg-gray-400" : "bg-transparent"
-      } fixed flex justify-between items-center w-full z-50 p-4 text-2xl`}
+      } fixed flex justify-between items-center w-full z-50 p-4 text-2xl lg:px-20`}
     >
       <p className="text-white z-10">
         <span className="">P</span>
         <span className="font-light">N</span>
         <span className="font-thin">C</span>
       </p>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={24}
-        height={15}
-        viewBox="0 0 24 15"
-        onClick={handleOpen}
-        className="z-50"
-      >
-        <line
-          id="Line_2"
-          data-name="Line 2"
-          x1={24}
-          transform="translate(0 0.5)"
-          fill="none"
-          stroke="#fff"
-          strokeWidth={1}
-        />
-        <line
-          id="Line_3"
-          data-name="Line 3"
-          x1={24}
-          transform="translate(0 7.5)"
-          fill="none"
-          stroke="#fff"
-          strokeWidth={1}
-        />
-        <line
-          id="Line_4"
-          data-name="Line 4"
-          x1={24}
-          transform="translate(0 14.5)"
-          fill="none"
-          stroke="#fff"
-          strokeWidth={1}
-        />
-      </svg>
+      {open ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={15}
+          viewBox="0 0 17.678 17.678"
+          onClick={handleOpen}
+          className="z-50 md:hidden"
+        >
+          <line
+            id="Line_7"
+            data-name="Line 7"
+            x1={24}
+            transform="translate(17.324 0.354) rotate(135)"
+            fill="none"
+            stroke="#fff"
+            strokeWidth={1}
+          />
+          <line
+            id="Line_8"
+            data-name="Line 8"
+            x1={24}
+            transform="translate(0.354 0.354) rotate(45)"
+            fill="none"
+            stroke="#fff"
+            strokeWidth={1}
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={15}
+          viewBox="0 0 24 15"
+          onClick={handleOpen}
+          className="z-50 md:hidden"
+        >
+          <line
+            id="Line_2"
+            data-name="Line 2"
+            x1={24}
+            transform="translate(0 0.5)"
+            fill="none"
+            stroke="#fff"
+            strokeWidth={1}
+          />
+          <line
+            id="Line_3"
+            data-name="Line 3"
+            x1={24}
+            transform="translate(0 7.5)"
+            fill="none"
+            stroke="#fff"
+            strokeWidth={1}
+          />
+          <line
+            id="Line_4"
+            data-name="Line 4"
+            x1={24}
+            transform="translate(0 14.5)"
+            fill="none"
+            stroke="#fff"
+            strokeWidth={1}
+          />
+        </svg>
+      )}
       <menu
         className={`transform ${
           open ? "-translate-x-0" : "translate-x-full"
-        } top-0 right-0 mt-0 w-5/12 fixed h-screen bg-gray-400 overflow-auto ease-in-out transition-all duration-300 lg:translate-x-0`}
+        } top-0 right-0 mt-0 w-5/12 lg:w-1/3 xl:w-1/4 fixed h-screen bg-gray-400 overflow-auto ease-in-out transition-all duration-300 md:block md:relative md:-translate-x-0 md:h-auto md:my-0 md:bg-transparent`}
       >
-        <ul className="text-white font-light text-lg mt-20 lg:mt-0 flex flex-col lg:flex-row justify-start w-full lg:mx-auto lg:flex lg:justify-between lg:max-w-screen-sm">
-          <li className="mb-4">Account</li>
-          <li className="mb-4">Teams</li>
-          <li className="mb-4">Payments</li>
+        <ul className="text-white font-light text-lg mt-20 flex flex-col justify-start w-full md:my-auto md:flex-row md:items-center md:justify-between">
+          <li className="mb-4 md:mb-0">Home</li>
+          <li className="mb-4 md:mb-0">About</li>
+          <li className="mb-4 bg-yellow-400 w-20 text-center px-4 py-1 rounded md:mb-0">
+            Login
+          </li>
         </ul>
       </menu>
     </nav>
