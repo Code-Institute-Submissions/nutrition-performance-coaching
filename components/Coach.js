44c18925f4db9d81689da2adadf86f1754b29334
Sam <hello@sammcnally.dev>
2021-06-23 21:40:22 +0100

    add: coaching responsiveness

--- components/Coach.js
+++ components/Coach.js
@@ -1,40 +1,50 @@
 import Image from "next/image";
+import ConsultationButon from "../components/ConsultationButton";
 
 const Coach = () => {
   return (
     <div className="py-12">
-      <h3 className="text-3xl mb-4 px-4">
-        Expert Programming by Experienced Nutritionists
-      </h3>
-      <h5 className="text-xl mb-4 font-light px-4">
-        Our lead nutritionist is Lindsey Doyle who has spent that past 5 years
-        coaching high performing athletes.
-      </h5>
-      <div className="relative flex">
-        <div className="bg-red-300 bg-opacity-70 w-full h-full p-4 absolute z-10">
-          <div className="text-white mt-44">
-            <h3 className="text-5xl mb-4">Lindsey Doyle</h3>
-            <hr className="mb-4" />
-
-            <p className="text-2xl mb-4">
-              MSc in Sports Nutrition and coach to many world class professional
-              MMA fighters and BJJ athletes
-            </p>
-          </div>
+      <div className="lg:flex lg:flex-row-reverse lg:items-center lg:bg-gray-400 mb-12">
+        <div className="md:mx-20 lg:mx-0 lg:w-7/12 lg:p-20 lg:text-white p-4">
+          <h3 className="lg:text-4xl mb-4 text-3xl">
+            Expert Programming by Experienced Nutritionists
+          </h3>
+          <h5 className="lg:text-2xl mb-4 text-xl font-light">
+            Our lead nutritionist is Lindsey Doyle who has spent that past 5
+            years coaching high performing athletes.
+          </h5>
         </div>
-        <Image
-          src={"/lindsey-nobg.png"}
-          width={500}
-          height={500}
-          objectFit="cover"
-          objectPosition="center"
-          className="w-full h-full"
-        />
+        <div className="lg:w-5/12 relative flex items-center justify-center">
+          <div className="bg-opacity-70 absolute z-10 flex flex-col items-center justify-center w-full h-full p-4 bg-red-300">
+            <div className="md:mx-20 text-white">
+              <h3 className="mb-4 text-5xl">Lindsey Doyle</h3>
+              <hr className="mb-4" />
+
+              <p className="mb-4 text-2xl">
+                MSc in Sports Nutrition and coach to many world class
+                professional MMA fighters and BJJ athletes
+              </p>
+            </div>
+          </div>
+          <Image
+            src={"/lindsey-nobg.png"}
+            width={500}
+            height={500}
+            objectFit="cover"
+            objectPosition="center"
+            className="w-full h-full mx-auto"
+          />
+        </div>
+        <div className="md:mx-20 lg:hidden p-4">
+          <h5 className=" mt-2 mb-4 text-xl font-light">
+            “I specialise in helping women make weight and perform in a safe,
+            efficient and effective manner!” - Lindsey
+          </h5>
+        </div>
       </div>
-      <h5 className="text-xl my-4 font-light px-4">
-        “I specialise in helping women make weight and perform in a safe,
-        efficient and effective manner!” - Lindsey
-      </h5>
+      <div className="md:mx-20 px-4">
+        <ConsultationButon />
+      </div>
     </div>
   );
 };
