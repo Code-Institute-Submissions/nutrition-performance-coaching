dd001dbde6527bbbc7c530afe17159e45443c651
Sam <hello@sammcnally.dev>
2021-06-23 21:40:49 +0100

    add: features responsivness

--- components/Features.js
+++ components/Features.js
@@ -4,40 +4,62 @@ import Perform from "../icons/Perform";
 
 const Features = () => {
   return (
-    <div className="px-4 pt-12 pb-6 text-gray-900">
-      <h3 className="text-3xl mb-4">Building better female atheltes</h3>
-      <h5 className="text-xl mb-8 font-light">
+    <div className="md:mx-20 px-4 py-12 text-gray-900">
+      <h3 className="lg:text-4xl mb-4 text-3xl">
+        Building better female atheltes
+      </h3>
+      <h5 className="lg:text-2xl mb-8 text-xl font-light">
         Our goal is to provide you with the best nutrition for the best
         performance through very simple process:
       </h5>
-      <div className="flex items-center justify-between mb-4">
-        <Journal />
-        <div className="w-1/2 pl-4">
-          <h5 className="text-xl mb-2 font-light">Journal</h5>
-          <p className="text-sm font-thin">
-            We create a simple food journal to gauge your current nutritional
-            preferences and habits.
-          </p>
+      <div className="xl:grid-cols-3 grid grid-cols-1">
+        <div className="grid-row-2 xl:grid-flow-row grid grid-cols-2 col-span-1 gap-1">
+          <div className="xl:order-last col-span-1 row-span-2">
+            <Journal />
+          </div>
+          <div className="xl:col-span-3 xl:order-first xl:justify-center xl:border-b flex items-end col-span-1 row-span-1">
+            <h5 className=" lg:text-3xl lg:mb-4 md:text-2xl text-xl font-light">
+              Journal
+            </h5>
+          </div>
+          <div className="xl:row-span-2 col-span-1 row-span-1">
+            <p className="md:text-base lg:text-xl xl:pr-8 xl:pt-6 text-sm font-thin">
+              We create a simple food journal to gauge your current nutritional
+              preferences and habits.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center justify-between mb-4">
-        <div className="w-1/2 pr-4">
-          <h5 className="text-xl mb-2 font-light">Program</h5>
-          <p className="text-sm font-thin">
-            We construct a program that is uniquely tailored to your performance
-            goals and timeline.
-          </p>
+        <div className="grid-row-2 xl:grid-flow-row grid grid-flow-col grid-cols-2 col-span-1 gap-1">
+          <div className="xl:order-last order-3 col-span-1 row-span-2">
+            <Program />
+          </div>
+          <div className="xl:col-span-3 xl:order-first xl:justify-center xl:border-b flex items-end order-1 col-span-1 row-span-1">
+            <h5 className="lg:text-3xl lg:mb-4 md:text-2xl text-xl font-light">
+              Program
+            </h5>
+          </div>
+          <div className="xl:row-span-2 order-2 col-span-1 row-span-1">
+            <p className="md:text-base lg:text-xl xl:pt-6 xl:pr-8 col-span-1 row-span-1 text-sm font-thin">
+              We construct a program that is uniquely tailored to your
+              performance goals and timeline.
+            </p>
+          </div>
         </div>
-        <Program />
-      </div>
-      <div className="flex items-center justify-between mb-4">
-        <Perform />
-        <div className="w-1/2 pl-4">
-          <h5 className="text-xl mb-2 font-light">Perform!</h5>
-          <p className="text-sm font-thin">
-            `We monitor your progress and make adaptations to your nutritional
-            programming as needed for optimal performance.
-          </p>
+        <div className="grid-row-2 xl:grid-flow-row grid grid-cols-2 col-span-1 gap-1">
+          <div className="xl:order-last col-span-1 row-span-2">
+            <Perform />
+          </div>
+          <div className="xl:col-span-3 xl:order-first xl:justify-center xl:border-b flex items-end col-span-1 row-span-1">
+            <h5 className=" lg:text-3xl lg:mb-4 md:text-2xl text-xl font-light">
+              Perform!
+            </h5>
+          </div>
+          <div className="xl:row-span-2 col-span-1 row-span-1">
+            <p className="md:text-base lg:text-xl xl:pr-8 xl:pt-6 text-sm font-thin">
+              We monitor your progress and make adaptations to your nutritional
+              programming as needed for optimal performance.
+            </p>
+          </div>
         </div>
       </div>
     </div>
